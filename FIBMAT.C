9ae4dd8f2e892d9abf51bf5de2956cf168e02c84
cyfin-cy <cyfingo@gmail.com>
2024-08-09 12:58:20 +0530

    Update FIBMAT.C

--- FIBMAT.C
+++ FIBMAT.C
@@ -137,3 +137,112 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/mman.h>
+
+void append_to_buffer(char *buffer, int *pos, const char *str) {
+    while (*str) {
+        buffer[*pos] = *str;
+        (*pos)++;
+        str++;
+    }
+    buffer[*pos] = '\0'; // Null-terminate the string
+}
+
+void int_to_str(int num, char *str) {
+    int i = 0, j, temp;
+    if (num == 0) {
+        str[i++] = '0';
+    } else {
+        while (num > 0) {
+            str[i++] = (num % 10) + '0';
+            num /= 10;
+        }
+        // Reverse the string
+        for (j = 0; j < i / 2; j++) {
+            temp = str[j];
+            str[j] = str[i - j - 1];
+            str[i - j - 1] = temp;
+        }
+    }
+    str[i] = ' '; // Add a space after the number
+    str[i + 1] = '\0'; // Null-terminate the string
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printf("\nNot passing k in the arguments\n");
+        exit(1);
+    }
+
+    int k = atoi(argv[1]);
+
+    if (k < 1)
+    {
+        printf("\nError in input\n");
+        exit(1);
+    }
+
+    int shmid = shmget(122, 4096, 0666 | IPC_CREAT);
+    if (shmid < 0)
+    {
+        perror("shmget failed");
+        exit(1);
+    }
+
+    void *psm = shmat(shmid, NULL, 0666);
+    if (psm == (void *)-1)
+    {
+        perror("shmat failed");
+        exit(1);
+    }
+
+    int n1 = 0, n2 = 1, n3;
+    char buffer[4096] = {0}; // Buffer to store the Fibonacci sequence
+    int pos = 0; // Current position in the buffer
+
+    char num_str[12]; // Buffer to convert integers to strings (assuming 32-bit int max length)
+
+    for (int i = 0; i < k; i++)
+    {
+        if (i == 0)
+        {
+            int_to_str(n1, num_str);
+        }
+        else if (i == 1)
+        {
+            int_to_str(n2, num_str);
+        }
+        else
+        {
+            n3 = n1 + n2;
+            int_to_str(n3, num_str);
+            n1 = n2;
+            n2 = n3;
+        }
+        append_to_buffer(buffer, &pos, num_str);
+    }
+
+    // Copy the buffer content to shared memory
+    char *dest = (char *)psm;
+    for (int i = 0; i <= pos; i++) {
+        dest[i] = buffer[i];
+    }
+
+    if (shmdt(psm) == -1)
+    {
+        perror("shmdt failed");
+        exit(1);
+    }
+
+    return 0;
+}
+
